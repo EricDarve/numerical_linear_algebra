7f28b505ca995b546fb57a8f6bc85717f1e9a89c
Eric Darve <darve@stanford.edu>
2016-08-01 16:48:18 -0700

    Added LU solvers

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,9 @@
 # Initialize the random number generator
-rng = MersenneTwister();
+rng = MersenneTwister()
 
-@assert 1 == 1
+# Triangular solvers
+
+include("../trtrs.jl")
 
 # Size of the matrix
 n = 32
@@ -18,12 +20,39 @@ end
 xe = rand(rng, 0:9, n) # This will be our solution
 b = L * xe
 
-# Load our triangular solvers
-include("../trtrs.jl")
-
 x = trtrsRow(L, b)
 # Let's check the result
 @assert x == xe
 
 x = trtrs(L, b)
 @assert x == xe
+
+
+# LU solvers
+include("getrf.jl")
+
+# Random initialization of matrix A
+L = zeros(Float64,n,n)
+U = zeros(Float64,n,n)
+for i=1:n
+    L[i,i] = 1
+    L[i+1:n,i] = rand(rng, -2:2, n-i)
+    U[i,i] = rand(rng, 1:2)
+    U[i,i+1:n] = rand(rng, -2:2, n-i)
+end
+A = L * U
+A0 = copy(A)
+
+# Initializing the right-hand side
+xe = rand(rng, 0:9, n) # This will be our solution
+b = A * xe
+
+# Test our solvers
+map([getrfOuter!, getrfAxpy!, getrfDot!]) do solver
+    A = copy(A0)
+    solver(A)
+    # Solve
+    x = getrs(A, b)
+    @assert x == xe
+    println(string(solver) * ": PASSED")
+end
