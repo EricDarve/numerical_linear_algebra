e6713d4ea191d2f3cf3565f4f4bea3cc475fb3bd
Eric Darve <darve@stanford.edu>
2016-08-01 12:03:37 -0700

    Added new code

---
+++ trtrs.jl
@@ -0,0 +1,24 @@
+function trtrsRow(L, b)
+    n = length(b)
+    x = Array{Float64}(n)
+    for i=1:n
+        z = 0.0
+        for j = 1:i-1
+            z += L[i,j] * x[j]
+        end
+        x[i] = (b[i] - z) / L[i,i]
+    end
+    return x
+end
+
+function trtrs(L, b)
+    n = length(b)
+    x = copy(b)
+    for j = 1:n
+        x[j] = x[j] / L[j,j]
+        for i = j+1:n
+            x[i] -= L[i,j] * x[j]
+        end
+    end
+    return x
+end
