a3cfdf845c799dec6a77f298c8a704f495352556
Eric Darve <darve@stanford.edu>
2016-08-28 16:58:56 -0700

    Updated installation script

--- install_all_packages.jl
+++ install_all_packages.jl
@@ -1,11 +1,12 @@
+if length(Base.LOAD_CACHE_PATH) >= 3
+    splice!(Base.LOAD_CACHE_PATH, 3)
+end
+
+println("[install_all_packages.jl] Installing HDF5")
+
 Pkg.add("HDF5")
 
-Pkg.add("PyPlot")
-Pkg.add("PlotlyJS")
-Pkg.add("PGFPlots")
-Pkg.add("Plots")
-
-# Pkg.update()
+println("[install_all_packages.jl] Testing HDF5")
 
 using HDF5
 n = 4
@@ -16,15 +17,18 @@ end
 A0 = h5read("test.h5", "A")
 @assert A == A0
 
+println("[install_all_packages.jl] Installation of HDF5 completed successfully!")
+
+# This completes the installation of HDF5
+
+println("[install_all_packages.jl] Installing Plots")
+
+Pkg.add("Plots")
+Pkg.add("PlotlyJS")
+
 using Plots
-
-pyplot()
-plot(rand(10)) 
-
 plotlyjs()
 plot(rand(10)) 
+# This completes the installation of Plots
 
-pgfplots()
-plot(rand(10)) 
-
-println("Installation completed successfully!")
+println("[install_all_packages.jl] Installation completed successfully!")
