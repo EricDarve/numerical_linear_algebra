124901fe4a5edff917377f86c12aa9985c860d20
Eric Darve <darve@stanford.edu>
2017-10-03 09:00:46 -0700

    Created src directory

---
+++ src/potrf.jl
@@ -0,0 +1,32 @@
+function potrf!(A)
+    n = size(A,1)
+    for j=1:n
+        for k=1:j-1, i=j:n
+            A[i,j] -= A[i,k] * A[j,k]
+        end
+        ajj = sqrt(A[j,j])
+        for i=j:n
+            A[i,j] /= ajj
+        end
+    end
+end
+
+function potrs(A,b)
+    n = length(b)
+    x = copy(b)
+    # Solve using matrix G
+    for j = 1:n
+        x[j] /= A[j,j]
+        for i = j+1:n
+            x[i] -= A[i,j] * x[j]
+        end
+    end
+    # Solve using matrix G^T
+    for i = n:-1:1
+        for j = i+1:n
+            x[i] -= A[j,i] * x[j]
+        end
+        x[i] /= A[i,i]
+    end
+    return x
+end
