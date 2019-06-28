0d659187effc28972f55d140bfb18947479df374
Eric Darve <darve@stanford.edu>
2019-06-28 16:08:25 -0700

    MINRES code update

--- Chap9/minres.jl
+++ Chap9/minres.jl
@@ -1,9 +1,38 @@
 using LinearAlgebra
 
+function symortho(a, b)
+    if (b == 0)
+        s = 0
+        r = abs(a)
+        if a == 0
+            c = 1
+        else
+            c = sign(a)
+        end
+    elseif a == 0
+        c = 0
+        r = abs(b)        
+        s = sign(b)
+    elseif abs(b) > abs(a)
+        tau = a / b
+        s = sign(b) / sqrt(1 + tau^2)
+        c = s * tau
+        r = b / s
+    else
+        tau = b / a
+        c = sign(a) / sqrt(1 + tau^2)
+        s = c * tau
+        r = a / c
+    end
+    return (c, s, r)
+end
+
 function minres(A, b;
     tol = sqrt(eps(real(eltype(b)))), # tolerance
     maxiter = length(b),              # maximum number of iterations
     precond = UniformScaling(1.0))     # preconditioner
+
+    println(".............V4.............")
 
     # It is assumed that the preconditioner supports the operator \
     # If you pass a matrix, we advise that you call lufact() before
@@ -18,53 +47,51 @@ function minres(A, b;
 
     M = precond # shorter name
 
-    v = M \ b
-    beta = norm(v)
-    v /= beta
-    vold = zeros(n)
+    q = M \ b
+    beta = norm(q)
+    q /= beta
+    qold = zeros(n)
 
     phi = beta
 
     c, s = -1., 0.
     delta = 0.
-    epsilon = 0.
-    dold, d = zeros(n), zeros(n)
+    
+    eps = 0.
+    pold, p = zeros(n), zeros(n)
 
     for i = 1:maxiter
         # Lanczos step
-        p = M \ (A * v)
-        alpha = dot(v, p)
-        p = p - alpha * v
-        vold, v = v, p - beta * vold
+        z = M \ (A * q)
+        alpha = dot(q, z) # Diagonal entry in Tk
+        qold, q = q, z - alpha * q - beta * qold
 
-        beta = norm(v)
+        beta = norm(q) # Off-diagonal entry in Tk
         if beta == 0.
             failure_message(i)
             return (x, res[1:i])
         end
 
-        v /= beta
+        q /= beta # Normalize q vector
 
-        # Solve min ||Tk' z - beta0 e1||
-        mu    = c * delta + s * alpha
+        # Solve min ||Tk' y - beta0 e1||
+        tau  = c * delta + s * alpha
         gamma = s * delta - c * alpha
 
         delta = - c * beta
-        epsilonold, epsilon = epsilon, s * beta
+        epsold, eps = eps, s * beta
 
-        # Build next rotation
-        eta = sqrt(gamma^2 + beta^2)
-        c = gamma / eta
-        s = beta  / eta
+        # Build rotation
+        c, s, eta = symortho(gamma, beta)
 
-        tau = c * phi
+        mu = c * phi
         phi = s * phi
 
-        # Update search direction d
-        dold, d = d, (vold - mu * d - epsilonold * dold) / eta
+        # Update search direction p
+        pold, p = p, (qold - tau * p - epsold * pold) / eta
 
         # Update solution
-        x += tau * d
+        x += mu * p
 
         # Check residual
         res[i + 1] = abs(phi)
